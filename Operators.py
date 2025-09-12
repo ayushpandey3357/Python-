864e05d9c24c6c54a2da8385f951c2782427d12d
Ayush Kumar Pandey <ayushpandey1974@gmail.com>
2025-09-12 22:26:08 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,111 @@
+# Question 1
+
+'''a = (input("Enter a number a : "))
+b = (input("Enter another number b: "))
+
+print(a + b)
+print(a - b)
+print(a*b)
+print(a / b)'''
+
+#question 2
+
+'''a = int (input("Enter a number: a"))
+b = int (input("Enter a number: b"))
+
+floor_divison = a // b
+remainder = a % b
+power = a ** b
+print(floor_divison,remainder,power)'''
+
+#question 3
+"""pen = 15
+notebook = 40
+totalcost = 3*pen + 2*notebook
+print(totalcost)"""
+
+#question 4
+"""a = int(input("enter a number a ="))
+b = int(input("enter a number b ="))
+c = int(input("enter a number c ="))
+d = int(input("enter a number d ="))
+e = int(input("enter a number e ="))
+
+avg = (a+b+c+d+e)/5
+print(avg)"""
+
+#question 5
+'''num = int(input("enter a number num ="))
+sqr = num*num
+cube = num*num*num
+sqrt = num*.5
+
+print("num :",num)
+print("sqrt :",sqr)
+print("cube :",cube)
+print("sqrt :",sqrt)'''
+
+
+#COMPARISON OPERATORS
+
+# question 1
+
+'''a = int(input("enter a number a ="))
+b = int(input("enter a number b ="))
+
+if(a==b):
+    print("a and b are equal")
+else:
+    print("a is not equal to b")"""
+
+# question 2
+"""a = int(input("enter a number a ="))
+b = int(input("enter a number b ="))
+
+if a > b:
+    print("a is greater :" ,a)
+else:
+    print("b is greater :" ,b)"""
+
+# Question 3
+
+"""num1 = int(input("enter a number num1 ="))
+num2 = int(input("enter a number num2 ="))
+num3 = int(input("enter a number num3 ="))
+
+if num1 > num2 and num1 > num3:
+    print("num1 is greater :",num1)
+
+elif num2 > num1 and num2 > num3:
+    print("num2 is greater :",num2)
+
+else:
+    print("num3 is greater :",num3)'''
+
+#Question 4
+
+'''marks = int(input("enter a number marks ="))
+if marks > 40 :
+    print("pass")
+else:
+    print("fail")'''
+
+# Question 5
+
+'''age = int(input("enter a age ="))
+if age >= 18:
+    print("Eligible to vote")
+
+else :
+    print("Not eligible to vote")'''
+
+# Question 6
+
+'''num1 = int (input("enter a number num1 ="))
+
+if (num1 % 2 == 0):
+    print("even =",num1)
+else :
+    print("odd =",num1)'''
+
+
